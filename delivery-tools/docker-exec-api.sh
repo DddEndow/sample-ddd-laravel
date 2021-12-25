8e475684ac0b39886a610be0163c13bc54b9f274
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-12-25 16:39:52 +0900

    [feat]dockerに入るようのコマンドを追加

---
+++ delivery-tools/docker-exec-api.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0) || exit
+cd ../delivery-docker || exit
+
+echo "+++++++++++++++++++++++++++++++++++++++"
+echo "Start to docker-compose exec delivery-api..."
+echo "+++++++++++++++++++++++++++++++++++++++"
+
+TMPDIR=/private$TMPDIR docker-compose exec delivery-api bash
