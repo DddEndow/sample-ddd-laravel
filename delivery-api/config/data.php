546546d6b1c533346838fd98cc18c528b8099d98
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-12-27 00:06:03 +0900

    [feat]laravel-dataの設定ファイルを追加

---
+++ delivery-api/config/data.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    /*
+     * The package will use this date format when working with dates through the app
+     */
+    'date_format' => DATE_ATOM,
+
+    /*
+     * Global transformers will take complex types and transform them into simple
+     * types.
+     */
+    'transformers' => [
+        DateTimeInterface::class => \Spatie\LaravelData\Transformers\DateTimeInterfaceTransformer::class,
+        \Illuminate\Contracts\Support\Arrayable::class => \Spatie\LaravelData\Transformers\ArrayableTransformer::class,
+    ],
+
+    /*
+     * Global casts will cast values into complex types when creating a data
+     * object from simple types.
+     */
+    'casts' => [
+        DateTimeInterface::class => Spatie\LaravelData\Casts\DateTimeInterfaceCast::class,
+    ],
+
+    /*
+     * Rule inferrers can be configured here. They will automatically add
+     * validation rules to properties of a data object based upon
+     * the type of the property.
+     */
+    'rule_inferrers' => [
+        Spatie\LaravelData\RuleInferrers\BuiltInTypesRuleInferrer::class,
+        Spatie\LaravelData\RuleInferrers\AttributesRuleInferrer::class,
+        Spatie\LaravelData\RuleInferrers\NullableRuleInferrer::class,
+        Spatie\LaravelData\RuleInferrers\RequiredRuleInferrer::class,
+    ],
+];
