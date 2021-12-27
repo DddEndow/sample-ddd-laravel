2ca6d446a25040cac6db8c4acea6adf02da39be0
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-12-27 18:59:44 +0900

    [feat]dbに接続するテストの基盤を作成

--- delivery-infra/tests/TestCase.php
+++ delivery-infra/tests/TestCase.php
@@ -12,7 +12,8 @@ class TestCase extends OrchestraTestCase
     public function setUp(): void
     {
         parent::setUp();
-        // additional setup
+
+        $this->artisan('migrate', ['--database' => 'testbench'])->run();
     }
 
     protected function getPackageProviders($app)
@@ -24,6 +25,26 @@ class TestCase extends OrchestraTestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        // perform environment setup
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+//            'options' => extension_loaded('pdo_mysql') ? array_filter([
+//                1009 => env('MYSQL_ATTR_SSL_CA'),
+//            ]) : [],
+        ]);
     }
 }
