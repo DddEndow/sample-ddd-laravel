f2eea967e192f991b746e2324b26a6e1b03444c7
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-12-30 15:30:55 +0900

    [feat]delivery-appパッケージを作成

---
+++ delivery-app/src/DeliveryAppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Delivery\App;
+
+use Illuminate\Support\ServiceProvider;
+
+class DeliveryAppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
