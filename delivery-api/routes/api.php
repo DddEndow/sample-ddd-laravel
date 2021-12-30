8560e2281763b4243686329b6565f896be31aca6
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-12-30 18:38:15 +0900

    [feat]拠点一覧を取得するエンドポイントを実装

--- delivery-api/routes/api.php
+++ delivery-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Hub\HubController;
 use App\Http\Controllers\Item\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/items', [ItemController::class, 'index']);
+
+Route::get('/hubs', [HubController::class, 'index']);
